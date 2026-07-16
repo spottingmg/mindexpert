3a18d43ad348c8fdf66ac1ea53326ba099136495
spottingmg <spottingmg@gmail.com>
2026-07-16 10:51:16 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "mindexpert",
+  "version": "1.0.0",
+  "type": "module",
+  "private": true,
+  "engines": { "node": ">=18" },
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.19.2"
+  }
+}
